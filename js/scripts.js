428eea195ba734f14fdcad56214fb004620057fc
Jacob Roberts <jacobroberts.dev@gmail.com>
2025-01-03 14:56:40 -0600

    Add handleClicks function and format with prettier

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
  */
 
 // Fetch products from the API and display them on the page
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     fetchAllProducts();
 });
 
@@ -11,17 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
 async function fetchAllProducts() {
     try {
         // Fetch all products
-        const response = await fetch('https://fakestoreapi.com/products');
+        const response = await fetch("https://fakestoreapi.com/products");
 
         // Throw an error if fetch fails
-        if (!response.ok) throw new Error(`Failed to fetch products. HTTP Status code: ${response.status}. Message: ${response.statusText}`);
+        if (!response.ok)
+            throw new Error(
+                `Failed to fetch products. HTTP Status code: ${response.status}. Message: ${response.statusText}`
+            );
 
         // Parse data and pass to helper functions
         const data = await response.json();
         displayProducts(data);
-        displayCategories(getAllCategories(data));
+        displayCategories(data, getAllCategories(data));
     } catch (error) {
-        // Handle error
+        // Handle and display error
         console.log(error);
         alert(error);
     }
@@ -29,36 +32,60 @@ async function fetchAllProducts() {
 
 // Get unique product categories from data returned by FetchAllProducts
 function getAllCategories(products) {
-    // Return array of unique categories
-    return [...new Set(products.map(product => product.category))];
+    // Return an array of unique categories
+    return [...new Set(products.map((product) => product.category))];
 }
 
 // Create button element for each category
 function createButtonElement(category) {
-    const button = document.createElement('button');
-
-    // Set button data attribute and text to category passed in
+    const button = document.createElement("button");
+    // Set button data attribute and text to the category passed in
     button.dataset.category = category;
     button.textContent = category;
-
     return button;
 }
 
-// Display categories on the page
-function displayCategories(categories) {
-    const categoryContainer = document.querySelector('.categories-container');
+// Display category buttons on the page
+function displayCategories(data, categories) {
+    // Get container element
+    const categoryContainer = document.querySelector(".categories-container");
+
+    // Create a button for each unique category and append to container
     categories.forEach((category) => {
         const button = createButtonElement(category);
         categoryContainer.appendChild(button);
     });
+
+    // Handle click events
+    handleClicks(data);
 }
 
+// Handle category button clicks and update product grid
+function handleClicks(data) {
+    // Get all category buttons
+    const buttons = document.querySelectorAll("button[data-category]");
+
+    // Setup click event for each button
+    buttons.forEach((button) =>
+        button.addEventListener("click", (e) => {
+            // Get category from target element data attribute
+            const selectedCategory = e.target.dataset.category;
+
+            if (selectedCategory === "all") {
+                displayProducts(data);
+            } else {
+                const relevantProducts = data.filter((product) => product.category === category);
+                displayProducts(relevantProducts);
+            }
+        })
+    );
+}
 
 // Display all products on the page based on the given data
 function displayProducts(products) {
-    const productGrid = document.querySelector('.product-grid');
-    productGrid.innerHTML = '';
-    products.forEach(product => {
+    const productGrid = document.querySelector(".product-grid");
+    productGrid.innerHTML = "";
+    products.forEach((product) => {
         const productElement = createProductElement(product);
         productGrid.appendChild(productElement);
     });
@@ -66,8 +93,8 @@ function displayProducts(products) {
 
 // Function to build each product card element
 function createProductElement(product) {
-    const productElement = document.createElement('article');
-    productElement.classList.add('product');
+    const productElement = document.createElement("article");
+    productElement.classList.add("product");
     productElement.innerHTML = `
         <img src="${product.image}" alt="${product.title}" class="product-image">
         <h2 class="title">${product.title}</h2>
